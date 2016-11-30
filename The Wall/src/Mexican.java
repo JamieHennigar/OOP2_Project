282d318b7f488176107b802304c1506ee807a697
JamieHennigar <jamiehennigar@gmail.com>
2016-11-30 00:23:02 +0000

    tidied up some of my code. added in some comments. started to do the KeyEventcode for moving my character.

--- The Wall/src/Mexican.java
+++ The Wall/src/Mexican.java
@@ -1,22 +1,38 @@
 
 public class Mexican 
 {
+	//declare attributes
 	private int health;
 	private int points;
 	
+	//no argument constructor
+	public Mexican()
+	{
+		health = 0;
+		points = 0;
+	}
+	
+	//multiple argument constructor
+	public Mexican(int health, int points)
+	{
+		this.health = health;
+		this.points = points;
+	}
+	
+	//mutators
 	public void setHealth(int health)
 	{
 		this.health = health;
 	}
 	
-	public int getHealth()
-	{
-		return health;
-	}
-	
 	public void setPoints(int points)
 	{
 		this.points = points;
+	}
+	
+	public int getHealth()
+	{
+		return health;
 	}
 	
 	public int getPoints()
